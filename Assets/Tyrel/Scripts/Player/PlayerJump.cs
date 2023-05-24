541b1f827876beb3d2c309ed4f1fc44779ef105b
xyndall <tyrel.ratima@gmail.com>
2023-05-24 16:13:16 +1200

    Working on Player input

---
+++ Assets/Tyrel/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    public float _moveSpeed = 5f;
+
+    public float jumpForce = 5f;
+    public float GroundDistance = .5f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+    [SerializeField] private PlayerInput playerInput;
+
+    private void Update()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+
+        Debug.DrawRay(groundCheck.position, -Vector3.up * GroundDistance, Color.cyan);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+
+    bool isGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(groundCheck.position, -Vector3.up * GroundDistance, out hit, 0.3f, ground))
+        {
+
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
---
+++ Assets/Tyrel/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    public float _moveSpeed = 5f;
+
+    [SerializeField] private Transform camFollow;
+    [SerializeField] private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+
+    Vector3 forwardRelativeVerticalInput;
+    Vector3 rightRelativeVerticalInput;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    public void Move()
+    {
+        
+
+        Vector2 InputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        //Vector3 forward = Camera.main.transform.forward;
+        //Vector3 right = Camera.main.transform.right;
+        //forward.y = 0;
+        //right.y = 0;
+        //forward = forward.normalized;
+        //right = right.normalized;
+
+        //forwardRelativeVerticalInput = InputVector.y * forward;
+        //rightRelativeVerticalInput = InputVector.x * right;
+
+        //Vector3 cameraRelativeMovement = forwardRelativeVerticalInput
+        //    + rightRelativeVerticalInput;
+        //_rb.AddForce(cameraRelativeMovement * _moveSpeed * Time.deltaTime, ForceMode.Impulse);
+
+
+        _rb.AddForce(new Vector3(InputVector.x, 0, InputVector.y) * _moveSpeed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
