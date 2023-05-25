88349112d0377797c06f6b094e16e2a6af9dfa8f
xyndall <tyrel.ratima@gmail.com>
2023-05-25 19:20:48 +1200

    bruh forgot to switch branch

---
+++ Assets/Tyrel/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    
+
+    public AudioSource[] aSource;
+
+    public AudioClip[] HitClip;
+    public AudioClip JumpClip;
+    public AudioClip DashClip;
+
+
+    public void PlayJump()
+    {
+
+
+    }
+    public void PlayDash()
+    {
+
+    }
+
+
+}
--- Assets/Tyrel/Scripts/Player/PlayerDash.cs
+++ Assets/Tyrel/Scripts/Player/PlayerDash.cs
@@ -14,10 +14,12 @@ public class PlayerDash : MonoBehaviour
     [SerializeField] private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
 
-    float timer;
+    [SerializeField] float timer;
+    public float DashCooldown = 2;
     int dashCounter = 1;
-    bool isDashinig;
-    float DashAmount = 10;
+    [SerializeField] bool isDashinig;
+    [SerializeField] bool TouchedGround;
+    public float DashAmount = 10;
 
     private void Awake()
     {
@@ -31,45 +33,62 @@ public class PlayerDash : MonoBehaviour
 
     private void Update()
     {
-
+        if (isGrounded())
+        {
+            TouchedGround = true;
+        }
+;
         if (isDashinig)
         {
             if (timer > 0)
             {
+                
                 timer -= Time.deltaTime;
             } 
             if (timer <= 0)
             {
+                
                 isDashinig = false;
-                timer = 2;
+                timer = DashCooldown;
+                
             }
         }
     }
 
     public void Dash(InputAction.CallbackContext context)
     {
-        if(context.performed && !isDashinig)
+        if (context.performed && !isDashinig && TouchedGround) 
         {
             Debug.Log("Dash");
 
             Vector2 InputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
-            Vector3 forward = Camera.main.transform.forward;
-            Vector3 right = Camera.main.transform.right;
-            forward.y = 0;
-            right.y = 0;
-            forward = forward.normalized;
-            right = right.normalized;
+            Vector3 CameraForward = Camera.main.transform.forward;
+            Vector3 CameraRight = Camera.main.transform.right;
+            CameraForward.y = 0;
+            CameraRight.y = 0;
+            CameraForward = CameraForward.normalized;
+            CameraRight = CameraRight.normalized;
 
-            Vector3 forwardRelativeVerticalInput = InputVector.y * forward;
-            Vector3 rightRelativeVerticalInput = InputVector.x * right;
+            Vector3 forwardRelativeVerticalInput = InputVector.y * CameraForward;
+            Vector3 rightRelativeVerticalInput = InputVector.x * CameraRight;
 
             Vector3 cameraRelativeMovement = forwardRelativeVerticalInput
                 + rightRelativeVerticalInput;
-            _rb.AddForce(cameraRelativeMovement.normalized * DashAmount * Time.deltaTime, ForceMode.Impulse);
+           
+
+            if(InputVector.x > 0 || InputVector.y > 0)
+            {
+                _rb.velocity = cameraRelativeMovement * DashAmount;
+            }
+            else if(InputVector.x <= 0 || InputVector.y <= 0)
+            {
+                _rb.velocity = CameraForward * DashAmount;
+            }
 
 
-            
+
+            TouchedGround = false;
             isDashinig = true;
             dashCounter--;
         }
